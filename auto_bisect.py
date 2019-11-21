9924f69dbbb3681983f34ad7c91ae343c943410a
Graham Van Goffrier <vangoffrier@gmail.com>
2019-11-21 12:30:05 +0000

    Testing commit-checking file

--- auto_bisect.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import subprocess
-
-if __name__ == "__main__":
-	subprocess.run(["git","bisect","start"])
-	subprocess.run(["git","bisect","good","4042fcf5"])
-	subprocess.run(["git","bisect","bad","master"])
-	subprocess.run(["git","bisect","run","python3","unit_test.py","2"])
